b6e91aeed8c589c6a8615fab33b06e7352dc3eca
nelson kigen <nellyk89@gmail.com>
2014-03-22 00:01:13 +0100

    Defined timestamp

---
+++ src/client/include/client_lego.h
@@ -0,0 +1,11 @@
+#ifndef __CLIENT_LEGO_H__
+#define __CLIENT_LEGO_H__
+
+#include<stdint.h>
+#include "kernel.h"
+#include "kernel_id.h"
+#include "ecrobot_interface.h"
+
+
+extern uint32_t timestamp;
+#endif
