5a05bcb2bbff491bf7e4f98b42ae2e428212835b
nelson kigen <nellyk89@gmail.com>
2014-03-21 19:56:22 +0100

    Bluetooth Discovery working now

--- src/server/include/server_bt.h
+++ src/server/include/server_bt.h
@@ -11,32 +11,34 @@
 #define MAX_INQUIRY_LEN		8
 #define MAX_BT_DEVICES		255 /*Max number of BT devices*/
 #define BT_NAME_LEN 		248 /*MAX length of user readable bluetooth device name*/
-#define BT_ADDR_LEN	19
+#define BT_ADDR_LEN		19
 
 /*
  * Structure representing a Bluetooth DEVICE
  */
 typedef struct {
     char name[BT_NAME_LEN]; /*user readable name of the bluetooth device*/
+    bdaddr_t device_addr; /*UNIQUE device address*/
 
-    baddr_t device_addr; /*UNIQUE device address*/
-
-} bt_device_t;
+}__attribute__((packed))  bt_device_t;
 
 /*
  * Search for available BLUETOOTH devices within range
  * upto a MAX of MAX_BT_DEVICES (defined above)
  */
-int bt_scan_devices(int s_sock,bt_device_t *devices[MAX_BT_DEVICES]);
+int bt_scan_devices(int dev_id, int s_sock,bt_device_t *devices[MAX_BT_DEVICES]);
 
 
 /*
  * Start Server Connection
  */
 
-int bt_start_server(int *s_sock); 
+int bt_start_server(int *s_sock);
 /*
  * Start a Bluetooth Connection
  */
 int bt_connect_device(int *c_sock,bdaddr_t *c_addr);
+
+ int bt_close_connection(int conn);
+
 #endif
--- src/server/server_bt.c
+++ src/server/server_bt.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
 
 #include "include/server_bt.h"
 
@@ -13,22 +18,36 @@ int bt_start_server(int *s_sock)
         perror("error opening socket\n");
         return -1;
     }
-    return 0;
+    return dev_id;
 }
 
-
-int bt_scan_devices(int s_sock,bt_device_t *devices[MAX_BT_DEVICES])
+int bt_connect_device(int *c_sock, bdaddr_t *c_addr)
 {
-    long flags;
+    struct sockaddr_rc addr = { 0 };
+
+    *c_sock = socket ( AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+
+    addr.rc_family = AF_BLUETOOTH;
+    addr.rc_channel = (uint8_t)1;
+    addr.rc_bdaddr = *c_addr;
+
+    return  connect(*c_sock, (struct sockaddr*)&addr,sizeof(struct sockaddr));
+
+}
+
+int bt_scan_devices(int dev_id, int s_sock, bt_device_t *devices[MAX_BT_DEVICES])
+{
+    int flags = IREQ_CACHE_FLUSH;
     int inq_res;
-    char addr[BT_ADDR_LEN] = {0};
-    char name[BT_NAME_LEN] = {0};
+    int i;
+    char addr[BT_ADDR_LEN] = { 0 };
+    char name[BT_NAME_LEN] = { 0 };
     inquiry_info *dev_in_range = NULL;
 
-    memset(devices, 0, MAX_BT_DEVICES * sizeof(bt_device_t));
+    //memset(devices, 0, MAX_BT_DEVICES * sizeof(bt_device_t));
     dev_in_range = (inquiry_info *) malloc(sizeof(inquiry_info) * MAX_BT_DEVICES);
 
-    inq_res = hci_inquiry(s_sock, MAX_INQUIRY_LEN, MAX_BT_DEVICES,NULL,&dev_in_range, IREQ_CACHE_FLUSH);
+    inq_res = hci_inquiry( dev_id, MAX_INQUIRY_LEN, MAX_BT_DEVICES, NULL, &dev_in_range, flags);
 
     if( inq_res < 0 )
     {
@@ -36,6 +55,8 @@ int bt_scan_devices(int s_sock,bt_device_t *devices[MAX_BT_DEVICES])
         return -1;
     }
     /*TODO: error when inq_res == 0*/
+
+    printf("BT_Server: # Devices found: %d\n",inq_res);
 
     for(i=0; i<inq_res; ++i)
     {
@@ -46,14 +67,21 @@ int bt_scan_devices(int s_sock,bt_device_t *devices[MAX_BT_DEVICES])
         {
             strcpy(name,"[unknown]\0");
         }
-
+          
         devices[i] = (bt_device_t *) malloc(sizeof(bt_device_t));
-	strcpy(&devices[i]->name, name);
-	memset(&devices[i]->device_addr,&dev_in_range[i].bdaddr,sizeof(bdaddr_t));
+        strcpy(devices[i]->name, name);
+        memcpy(&devices[i]->device_addr,&dev_in_range[i].bdaddr,sizeof(bdaddr_t));
+
+	printf("BT_Server: Device #: %d ; Device ID: %s ; Device Name: %s ;\n",i, addr,devices[i]->name);
     }
 
     free(dev_in_range);
     close(s_sock);
-    
+
     return inq_res;
+}
+
+int bt_close_connection(int conn)
+{
+    return close(conn);
 }
