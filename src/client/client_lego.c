333e4318c8320c214498605f3bdb424a2a42281a
nelson kigen <nellyk89@gmail.com>
2014-03-22 22:24:29 +0100

    Data structs initilizations

--- src/client/client_lego.c
+++ src/client/client_lego.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 #include "include/client_lego.h"
-//#include "../common/include/"
-/*************GLOBAL VARIABLES**********/
+#include "../common/include/bt_packet.h"
 
+
+/*************GLOBAL VARIABLES**********/
 uint32_t timestamp;
+bt_packet_t incoming[MAX_REQ];
+bt_packet_t outgoing[MAX_REQ];
+
 #define DEVICE_PWD "1234"
 
 /******OSEK Declarations******/
@@ -17,7 +23,11 @@ DeclareAlarm(LCD_UPDATE_ALARM);
 DeclareCounter(SysTimerCnt);
 
 
-
+static void reset_data_structs()
+{
+    memset(incoming, 0, sizeof(bt_packet_t));
+    memset(outgoing, 0, sizeof(bt_packet_t));
+}
 
 static void reset_motor_power()
 {
@@ -25,11 +35,14 @@ static void reset_motor_power()
     nxt_motor_set_speed(NXT_PORT_B, 0, 1);
     nxt_motor_set_speed(NXT_PORT_C, 0, 1);
 }
-/****Initialize DEVICE*****/
 
+
+/****Initialize DEVICE*****/
 void ecrobot_device_initialize()
 {
     timestamp = 0;
+    reset_data_structs();
+    reset_motor_power();
     ecrobot_init_bt_slave(DEVICE_PWD);
 }
 
@@ -37,6 +50,7 @@ void ecrobot_device_initialize()
 void ecrobot_device_terminate()
 {
     reset_motor_power();
+    reset_data_structs();
     bt_reset();
     ecrobot_term_bt_connection();
 }
