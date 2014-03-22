e6955c829c45b43fe9df5d914297c741d4d28646
nelson kigen <nellyk89@gmail.com>
2014-03-22 15:03:10 +0100

    -Additional function definitions

--- src/client/client_lego.c
+++ src/client/client_lego.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 #include "include/client_lego.h"
-
+//#include "../common/include/"
 /*************GLOBAL VARIABLES**********/
 
 uint32_t timestamp = 0;
-
+#define DEVICE_PWD "1234"
 
 /******OSEK Declarations******/
 
@@ -15,6 +15,22 @@ DeclareResource(RES_LCD);
 DeclareAlarm(BT_COMM_ALARM);
 DeclareAlarm(LCD_UPDATE_ALARM);
 DeclareCounter(SysTimerCnt);
+
+
+/****Initialize DEVICE*****/
+
+void ecrobot_device_initialize()
+{
+    ecrobot_init_bt_slave(DEVICE_PWD);
+}
+
+
+void ecrobot_device_terminate()
+{
+
+    bt_reset();
+    ecrobot_term_bt_connection();
+}
 
 
 void user_1ms_isr_type2(void)
@@ -33,10 +49,12 @@ void user_1ms_isr_type2(void)
 TASK(BtComm)
 {
 
+    TerminateTask();
 }
 
 
 TASK(DisplayTask)
 {
 
+    TerminateTask();
 }
