f8bd3f7d229f0b7e63d6a23d76f387aa90c7f8e1
nelson kigen <nellyk89@gmail.com>
2014-03-23 16:38:17 +0100

    C client for the server

---
+++ src/app/c_app/c-app.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "include/c-app.h"
+#include "../server/include/controller.h"
+#include "../common/include/device_operations.h"
+
+
+int start_app()
+{
+
+}
---
+++ src/app/c_app/include/c-app.h
@@ -0,0 +1,9 @@
+#ifndef __C_APP_H__
+#define __C_APP_H__
+
+#include "../server/include/controller.h"
+#include "../common/include/device_operations.h"
+#include "../common/include/opt_decoder.h"
+
+int start_app(char *data);
+#endif
