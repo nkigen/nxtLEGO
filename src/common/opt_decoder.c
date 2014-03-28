b587cd181726aeeb66d8802f1c0688c115862b43
nelson kigen <nellyk89@gmail.com>
2014-03-28 14:15:12 +0100

    Decoder in perfect condition

--- src/common/opt_decoder.c
+++ src/common/opt_decoder.c
@@ -11,22 +11,25 @@ static int decode_motor_power(char *str, motor_opts_t *motor)
     char tmp[5]; /*Maximum 3 digits+ 1 for the sign!!*/
     int val;
     int len;
-
+    char *cp=p;
     if(( p = strchr(p, ':')) == NULL)
         return -1;
     len = p - &str[0];
-    memcpy(tmp, str, len - 1);
+    memcpy(tmp, str, len);
     sscanf(tmp, "%d", &val); /*TODO: check if > 0*/
     motor->min_power = val;
-    p += len + 1;
+    p += len;
+    cp = p;
     memset(tmp, 0, 5);
     if((p = strchr(p, ':')) == NULL)
         return -1;
     len = p - &str[0];
-    memcpy(tmp,str, len - 1);
+    int sz = p - cp;
+    memcpy(tmp, cp , sz);
+    cp = p;
     sscanf(tmp,"%d",&val); /*TODO: check if > 0*/
     motor->max_power = val;
-    p += len + 1;
+    p += 1;
     sscanf(p,"%d",&val);
     motor->step = val;
     return 0;
@@ -45,18 +48,21 @@ static int decode_motor_opts(char *str, motor_opts_t *motor)
         {
         case OPT_PORT:
             ++isok;
+            ++p;
             sscanf(p,"%d%n",&tmp,&n);
             motor->port = tmp;
             p += n;
             break;
         case OPT_MOTOR_POWER:
             ++isok;
+            ++p;
             sscanf(p,"%s%n",buf,&n);
             p += n;
             decode_motor_power(buf,motor);
             break;
         case OPT_MOTOR_SAMPLE:
             ++isok;
+            ++p;
             sscanf(p,"%d%n",&tmp,&n);
             motor->num_samples = tmp;
             p += n;
@@ -66,6 +72,8 @@ static int decode_motor_opts(char *str, motor_opts_t *motor)
         }
 
     }
+    
+    printf("min_power: %d , max_power: %d, step: %d samples %d\n", motor->min_power,motor->max_power,motor->step, motor->num_samples);
     if(isok == NUM_MOTOR_OPTS) /*All options are OK*/
         return 0;
     else         /*Sad case: Some important options are missing :-(*/
@@ -75,13 +83,13 @@ static int decode_motor_opts(char *str, motor_opts_t *motor)
 int decode_options(char *str, app_options_t *opts)
 {
     int sz;
-    motor_opts_t motor;
-    memset(&motor, 0 , sizeof(motor_opts_t));
-    sz = decode_motor_opts(str, &motor);
+    sz = decode_motor_opts(str, &opts->motor);
     if(sz < 0)
     {
         perror("failed to decode options string");
         return -1;
     }
+    else
+        printf("Options decoded successfully\n");
     return 0;
 }
