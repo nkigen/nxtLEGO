063abfc1edd8d6d87559e597c37508bacf55bc14
nelson kigen <nellyk89@gmail.com>
2014-09-11 15:54:18 +0200

    Unicycle modifications, minor updates

--- src/client/controller.c
+++ src/client/controller.c
@@ -6,10 +6,13 @@
 MOTOR_CONTROLLER left_motor;
 MOTOR_CONTROLLER right_motor;
 
+UNICYCLE_CONTROLLER unicycle_controller;
+
+
 /*Convert degrees to radians*/
 double toRadians(int rad)
 {
-    return ( ( rad*M_PI) / 180);
+    return ( ( rad*MY_PI) / 180);
 }
 
 /*Saturate power values*/
@@ -26,7 +29,7 @@ void init_controller(MOTOR_CONTROLLER *c) {
     c->e1 = 0;
     c->e2 = 0;
     c->u = 0;
-    c-u1 = 0;
+    c->u1 = 0;
     c->u2 = 0;
     c->dPrev = 0;
     c->cVel = 0;
@@ -34,12 +37,29 @@ void init_controller(MOTOR_CONTROLLER *c) {
     c->fPrev = 0;
 }
 
+void initUnicycle(UNICYCLE_CONTROLLER *uc){
+uc->cPos = 0;
+}
+void calcDesiredVelocity(MOTOR_CONTROLLER *rm, MOTOR_CONTROLLER *lm, UNICYCLE_CONTROLLER *uc){
+
+	/*Left Motor*/
+	lm->dVel = ((2 * V) + (uc->cPos * D)) /(2 * R);
+	/*right Motor*/
+	rm->dVel = ((2 * V) - (uc->cPos * D)) /(2 * R); 
+}
 /*Output: desired power*/
 double controllerUpdate(MOTOR_CONTROLLER *c, double error) {
     /*TODO:implement this*/
+	return 0.0;
 }
 
 
+/**
+ * The Unicycle Controller implementation
+ */
+double unicycleUpdate(UNICYCLE_CONTROLLER *uc, double error){
+return 0.0;
+}
 double derivative(MOTOR_CONTROLLER *c, double val) {
     c->dPrev = (val - c->dPrev)/ T;
     return c->dPrev;
